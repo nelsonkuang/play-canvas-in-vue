a07b18b448c397181d26d806fa760c5b78f4d328
nelsonkuang <410374308@qq.com>
2020-07-10 09:28:02 +0800

    优化拖拉效果

--- src/views/demo-1/index.vue
+++ src/views/demo-1/index.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ $route.meta.title }}</h1>
     <div class="container">
-      <canvas id="canvas" ref="canvas" class="canvas" :class="{'hover' : hoverDisplayObject}" width="500" height="500"></canvas>
+      <canvas id="canvas" ref="canvas" class="canvas" width="500" height="500"></canvas>
     </div>
   </div>
 </template>
@@ -11,149 +11,165 @@
 /* eslint-disable no-alert, no-console */
 import Path from '../../utils/classes/Path'
 import { getDomOffset } from '../../utils/tools'
+const curve = {
+  canvas: null,
+  path: new Path(),
+  p0: [150, 50],
+  p1: [160, 100],
+  p2: [200, 120],
+  p3: [250, 90],
+  r: 5,
+  currentPos: {
+    x: 0,
+    y: 0
+  },
+  animationID: null
+}
 export default {
   data () {
     return {
-      canvas: null,
-      path: new Path(),
-      p0: [150, 50],
-      p1: [160, 100],
-      p2: [200, 120],
-      p3: [250, 90],
-      r: 5,
-      currentPos: {
-        x: 0,
-        y: 0
-      },
-      animationID: null
     }
   },
   computed: {
-    displayObjects () {
-      const { p0, p1, p2, p3, r } = this
-      return {
-        p0: {
-          name: 'p0',
-          x: p0[0] - r,
-          y: p0[1] - r,
-          width: 2 * r,
-          height: 2 * r,
-          zIndex: 0
-        },
-        p1: {
-          name: 'p1',
-          x: p1[0] - r,
-          y: p1[1] - r,
-          width: 2 * r,
-          height: 2 * r,
-          zIndex: 1
-        },
-        p2: {
-          name: 'p2',
-          x: p2[0] - r,
-          y: p2[1] - r,
-          width: 2 * r,
-          height: 2 * r,
-          zIndex: 2
-        },
-        p3: {
-          name: 'p3',
-          x: p3[0] - r,
-          y: p3[1] - r,
-          width: 2 * r,
-          height: 2 * r,
-          zIndex: 3
-        }
-      }
-    },
-    hoverDisplayObject () {
-      const { currentPos, displayObjects } = this
-      const { p0, p1, p2, p3 } = displayObjects
-      if (currentPos.x > p0.x && currentPos.x < p0.x + p0.width && currentPos.y > p0.y && currentPos.y < p0.y + p0.height) {
-        return p0
-      } else if (currentPos.x > p1.x && currentPos.x < p1.x + p1.width && currentPos.y > p1.y && currentPos.y < p1.y + p1.height) {
-        return p1
-      } else if (currentPos.x > p2.x && currentPos.x < p2.x + p2.width && currentPos.y > p2.y && currentPos.y < p2.y + p2.height) {
-        return p2
-      } else if (currentPos.x > p3.x && currentPos.x < p3.x + p3.width && currentPos.y > p3.y && currentPos.y < p3.y + p3.height) {
-        return p3
-      } else {
-        return null
-      }
-    }
   },
   mounted () {
-    this.canvas = this.$refs.canvas
-    this.draw()
-    this.bindEvents()
+    this.initCavas()
   },
   beforeDestroy () {
-    cancelAnimationFrame(this.animationID)
+    cancelAnimationFrame(curve.animationID)
   },
   methods: {
-    draw () {
-      const ctx = this.canvas.getContext('2d')
-      const cWidth = Number(this.canvas.getAttribute('width'))
-      const cHeight = Number(this.canvas.getAttribute('height'))
-      ctx.clearRect(0, 0, cWidth, cHeight)
-      ctx.strokeStyle = '#000000'
-      const { p0, p1, p2, p3, r } = this
-      const p = this.path
-      p.beginPath(ctx)
-      // 画线
-      p.moveTo(p0[0], p0[1])
-      p.bezierCurveTo(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1])
-      p.stroke(ctx)
-      p.closePath()
-      p.beginPath(ctx)
-      ctx.strokeStyle = '#cccccc'
-      p.moveTo(p0[0], p0[1])
-      p.lineTo(p1[0], p1[1])
-      p.moveTo(p3[0], p3[1])
-      p.lineTo(p2[0], p2[1])
-      // 画点
-      p.stroke(ctx)
-      p.closePath()
-      p.beginPath(ctx)
-      ctx.strokeStyle = '#000000'
-      p.moveTo(p0[0] + r, p0[1])
-      p.arc(p0[0], p0[1], r, 0, Math.PI * 2, false)
-      p.moveTo(p3[0] + r, p3[1])
-      p.arc(p3[0], p3[1], r, 0, Math.PI * 2, false)
-      p.moveTo(p1[0] + r, p1[1])
-      p.arc(p1[0], p1[1], r, 0, Math.PI * 2, false)
-      p.moveTo(p2[0] + r, p2[1])
-      p.arc(p2[0], p2[1], r, 0, Math.PI * 2, false)
+    initCavas () {
+      curve.canvas = this.$refs.canvas
+      const draw = () => {
+        const ctx = curve.canvas.getContext('2d')
+        const cWidth = Number(curve.canvas.getAttribute('width'))
+        const cHeight = Number(curve.canvas.getAttribute('height'))
+        ctx.clearRect(0, 0, cWidth, cHeight)
+        ctx.strokeStyle = '#000000'
+        const { p0, p1, p2, p3, r } = curve
+        const p = curve.path
+        p.beginPath(ctx)
+        // 画线
+        p.moveTo(p0[0], p0[1])
+        p.bezierCurveTo(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1])
+        p.stroke(ctx)
+        p.closePath()
+        p.beginPath(ctx)
+        ctx.strokeStyle = '#cccccc'
+        p.moveTo(p0[0], p0[1])
+        p.lineTo(p1[0], p1[1])
+        p.moveTo(p3[0], p3[1])
+        p.lineTo(p2[0], p2[1])
+        // 画点
+        p.stroke(ctx)
+        p.closePath()
+        p.beginPath(ctx)
+        ctx.strokeStyle = '#000000'
+        p.moveTo(p0[0] + r, p0[1])
+        p.arc(p0[0], p0[1], r, 0, Math.PI * 2, false)
+        p.moveTo(p3[0] + r, p3[1])
+        p.arc(p3[0], p3[1], r, 0, Math.PI * 2, false)
+        p.moveTo(p1[0] + r, p1[1])
+        p.arc(p1[0], p1[1], r, 0, Math.PI * 2, false)
+        p.moveTo(p2[0] + r, p2[1])
+        p.arc(p2[0], p2[1], r, 0, Math.PI * 2, false)
 
-      p.stroke(ctx)
-      p.closePath()
-      this.animationID = requestAnimationFrame(this.draw)
-    },
-    bindEvents () {
-      const This = this
-      const offset = getDomOffset(This.canvas)
-      let isDraging = false
-      This.canvas.onmousemove = function (event) {
-        This.currentPos = { x: event.pageX - offset.left, y: event.pageY - offset.top }
-        if (This.hoverDisplayObject && isDraging) {
-          let p = []
-          p[0] = This.currentPos.x
-          p[1] = This.currentPos.y
-          This[This.hoverDisplayObject.name] = p
+        p.stroke(ctx)
+        p.closePath()
+      }
+      const bindEvents = () => {
+        const This = curve
+        const offset = getDomOffset(This.canvas)
+        let isDraging = false
+        This.canvas.onmousemove = function (event) {
+          This.currentPos = { x: event.pageX - offset.left, y: event.pageY - offset.top }
+          const hoverDisplayObject = getHoverDisplayObject()
+          if (hoverDisplayObject && isDraging) {
+            let p = []
+            p[0] = This.currentPos.x
+            p[1] = This.currentPos.y
+            This[hoverDisplayObject.name] = p
+          }
+        }
+        This.canvas.onmousedown = function (event) {
+          This.currentPos = { x: event.pageX - offset.left, y: event.pageY - offset.top }
+          if (getHoverDisplayObject()) {
+            isDraging = true
+          }
+        }
+        This.canvas.onmouseup = function () {
+          isDraging = false
+        }
+        This.canvas.onmouseleave = function () {
+          isDraging = false
         }
       }
-      This.canvas.onmousedown = function (event) {
-        This.currentPos = { x: event.pageX - offset.left, y: event.pageY - offset.top }
-        if (This.hoverDisplayObject) {
-          isDraging = true
+      const getDisplayObjects = () => {
+        const { p0, p1, p2, p3, r } = curve
+        return {
+          p0: {
+            name: 'p0',
+            x: p0[0] - r,
+            y: p0[1] - r,
+            width: 2 * r,
+            height: 2 * r,
+            zIndex: 0
+          },
+          p1: {
+            name: 'p1',
+            x: p1[0] - r,
+            y: p1[1] - r,
+            width: 2 * r,
+            height: 2 * r,
+            zIndex: 1
+          },
+          p2: {
+            name: 'p2',
+            x: p2[0] - r,
+            y: p2[1] - r,
+            width: 2 * r,
+            height: 2 * r,
+            zIndex: 2
+          },
+          p3: {
+            name: 'p3',
+            x: p3[0] - r,
+            y: p3[1] - r,
+            width: 2 * r,
+            height: 2 * r,
+            zIndex: 3
+          }
         }
       }
-      This.canvas.onmouseup = function () {
-        isDraging = false
+      const getHoverDisplayObject = () => {
+        const { currentPos } = curve
+        const { p0, p1, p2, p3 } = getDisplayObjects()
+        if (currentPos.x > p0.x && currentPos.x < p0.x + p0.width && currentPos.y > p0.y && currentPos.y < p0.y + p0.height) {
+          return p0
+        } else if (currentPos.x > p1.x && currentPos.x < p1.x + p1.width && currentPos.y > p1.y && currentPos.y < p1.y + p1.height) {
+          return p1
+        } else if (currentPos.x > p2.x && currentPos.x < p2.x + p2.width && currentPos.y > p2.y && currentPos.y < p2.y + p2.height) {
+          return p2
+        } else if (currentPos.x > p3.x && currentPos.x < p3.x + p3.width && currentPos.y > p3.y && currentPos.y < p3.y + p3.height) {
+          return p3
+        } else {
+          return null
+        }
       }
-      This.canvas.onmouseleave = function () {
-        isDraging = false
+      const update = () => {
+        const hoverDisplayObject = getHoverDisplayObject()
+        if (hoverDisplayObject) {
+          curve.canvas.classList.add('hover')
+        } else {
+          curve.canvas.classList.remove('hover')
+        }
+        draw()
+        curve.animationID = requestAnimationFrame(update)
       }
+
+      bindEvents()
+      update()
     }
   }
 }
