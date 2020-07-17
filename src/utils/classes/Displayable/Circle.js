3f170aec8a76ff365b73f7954c52195367ff1031
nelsonkuang <410374308@qq.com>
2020-07-17 10:25:21 +0800

    增加Circle

---
+++ src/utils/classes/Displayable/Circle.js
@@ -0,0 +1,30 @@
+import DisplayableObject from './Object'
+
+class DisplayableCircle extends DisplayableObject {
+
+  constructor({ fillStyle = '#ffffff', strokeStyle = '#000000', lineWidth = 1, x = 0, y = 0, radius = 10, zIndex = 0 }) {
+    super(x - radius, y - radius, radius * 2, radius * 2, zIndex)
+    this.fillStyle = fillStyle
+    this.strokeStyle = strokeStyle
+    this.lineWidth = lineWidth
+    this.radius = radius
+  }
+
+  draw (ctx) {
+    const { x, y, radius, fillStyle, strokeStyle, lineWidth } = this
+    const m = this.getMatrix()
+    ctx.save()
+    ctx.setTransform(m[0], m[1], m[2], m[3], m[4], m[5])
+    ctx.fillStyle = fillStyle
+    ctx.strokeStyle = strokeStyle
+    ctx.lineWidth = lineWidth
+    ctx.beginPath()
+    ctx.arc(x + radius, y + radius, radius, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+    ctx.restore()
+  }
+}
+
+export default DisplayableCircle
