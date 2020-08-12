0043ade9cc931e027722183b4faba775b97432a4
nelsonkuang <410374308@qq.com>
2020-08-12 11:45:27 +0800

    优化纹理

--- src/views/demo-9/index.vue
+++ src/views/demo-9/index.vue
@@ -92,7 +92,16 @@ export default {
       // Now that the image has loaded make copy it to the texture.
       gl.bindTexture(gl.TEXTURE_2D, texture)
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
-      gl.generateMipmap(gl.TEXTURE_2D)
+      // Check if the image is a power of 2 in both dimensions.
+      if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+        // Yes, it's a power of 2. Generate mips.
+        gl.generateMipmap(gl.TEXTURE_2D)
+      } else {
+        // No, it's not a power of 2. Turn off mips and set wrapping to clamp to edge
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+      }
     })
 
     /* =================== 通用函数 =================== */
@@ -119,6 +128,9 @@ export default {
         img.onerror = null
         cb(img)
       }
+    }
+    function isPowerOf2 (value) {
+      return (value & (value - 1)) === 0
     }
     // Fill the buffer with the values that define a letter 'F'.
     function setGeometry (gl) {
@@ -620,3 +632,10 @@ export default {
   }
 }
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.canvas {
+  background-color: #fff;
+}
+</style>
+
