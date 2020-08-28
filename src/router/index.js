37ec066f85ea86120259039732a0a0b3c908be76
nelsonkuang <410374308@qq.com>
2020-08-28 16:06:11 +0800

    平行光源阴影的实现

--- src/router/index.js
+++ src/router/index.js
@@ -275,6 +275,14 @@ const routes = [
     meta: {
       title: 'Webgl 教程 点击光源阴影的实现'
     }
+  },
+  {
+    path: '/demo-33',
+    name: 'Demo33',
+    component: () => import('@/views/demo-33/index'),
+    meta: {
+      title: 'Webgl 教程 平行光源阴影的实现'
+    }
   }
 ]
 
