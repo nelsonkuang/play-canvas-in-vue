7155143c975b6c91b0ead9c6529634929619432c
nelsonkuang <410374308@qq.com>
2020-08-31 23:35:06 +0800

    同上仍需优化

--- src/views/demo-35/index.vue
+++ src/views/demo-35/index.vue
@@ -7,7 +7,7 @@
 // Reference from: 
 // https://webglfundamentals.org/webgl/lessons/zh_cn/webgl-skybox.html
 // https://zhoyq.github.io/panoramic
-import { mat4 } from 'gl-matrix'
+import { mat4, vec3 } from 'gl-matrix'
 import { createProgramInfo, setUniforms, setBuffersAndAttributes, drawBufferInfo } from '../../utils/tools/web-gl'
 import { createBufferInfoFunc, createCubeVertices } from '../../utils/tools/primitives'
 const negXImg = './static/img/skybox/neg-x.jpg'
@@ -154,17 +154,27 @@ export default {
     //   return Math.floor(Math.random() * range)
     // }
 
+    const originCameraPosition = vec3.fromValues(0, 0, 2)
+    let cameraPosition = vec3.fromValues(0, 0, 2)
+    const target = vec3.fromValues(0, 0, 0)
+    const up = vec3.fromValues(0, 1, 0)
     const fieldOfViewRadians = degToRad(60)
-    // const cameraYRotation = degToRad(0)
+    let theta = 0 // x 方向
+    let phi = 0 // y 方向
+    let dX = 0
+    let dY = 0
+    let drag = false
+    let zoom = 1
+    let time = 0
+    const supportedTouch = window.hasOwnProperty('ontouchstart')
 
-    // let then = 0
 
-    drawScene(0)
+    drawScene()
 
     // Draw the scene.
-    function drawScene (time) {
+    function drawScene () {
       // Convert to seconds
-      time *= 0.0001
+      !drag && (time += 0.0001)
 
       // Tell WebGL how to convert from clip space to pixels
       gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
@@ -184,9 +194,7 @@ export default {
 
       // Compute the camera's matrix
       // 相机在以原点为圆心直径2个单位的圆上看向原点
-      const cameraPosition = [Math.cos(time) * 2, 0, Math.sin(time) * 2]
-      const target = [0, 0, 0]
-      const up = [0, 1, 0]
+      // const cameraPosition = [Math.cos(time) * 2, 0, Math.sin(time) * 2]
       // 2. Compute the view's matrix using look at directly.
       const viewDirectionMatrix = mat4.create()
       mat4.lookAt(viewDirectionMatrix, cameraPosition, target, up)
@@ -216,6 +224,116 @@ export default {
 
       animationID = requestAnimationFrame(drawScene)
     }
+    function zoomInCamera (delta) {
+      if (delta > 0) {
+        zoom *= 1.04
+      } else {
+        zoom /= 1.04
+      }
+    }
+    function rotateCamera (dx, dy) {
+      const phiMax = Math.PI / 2 - 0.01
+      theta += dx
+      phi += dy
+      phi = Math.min(Math.max(phi, - Math.PI / 2), phiMax)
+    }
+    function updateCamera () {
+      // modelRotation = [phi, theta]
+      vec3.copy(cameraPosition, originCameraPosition)
+      vec3.rotateX(cameraPosition, cameraPosition, target, phi)
+      vec3.rotateY(cameraPosition, cameraPosition, target, theta)
+      vec3.scale(cameraPosition, cameraPosition, zoom)
+
+      drawScene()
+    }
+    /* ================= Mouse events ====================== */
+    function bindMouseEvents () {
+      let oldX = 0
+      let oldY = 0
+
+      const mouseDown = function (e) {
+        e.preventDefault()
+        drag = true
+        oldX = e.pageX
+        oldY = e.pageY
+        return false
+      }
+
+      const mouseUp = function () {
+        drag = false
+      }
+
+      const mouseMove = function (e) {
+        if (!drag) {
+          canvas.style.cursor = 'grab'
+          return
+        }
+        e.preventDefault()
+        dX = (e.pageX - oldX) * 2 * Math.PI / gl.canvas.clientWidth
+        dY = (e.pageY - oldY) * 2 * Math.PI / gl.canvas.clientHeight
+        oldX = e.pageX
+        oldY = e.pageY
+        rotateCamera(dX, dY)
+        updateCamera()
+      }
+      const mouseWheel = function (e) {
+        if (Math.abs(e.deltaY) < 1.0) {
+          return
+        }
+
+        canvas.style.cursor = 'none'
+        zoomInCamera(e.deltaY)
+        updateCamera()
+      }
+
+      canvas.addEventListener('mousedown', mouseDown, false)
+      canvas.addEventListener('mouseup', mouseUp, false)
+      canvas.addEventListener('mouseout', mouseUp, false)
+      canvas.addEventListener('mousemove', mouseMove, false)
+      canvas.addEventListener('wheel', mouseWheel, false)
+    }
+    /* ================= Touch events ====================== */
+    function bindTouchEvents () {
+      let oldX = 0
+      let oldY = 0
+      let currentDistance = 0
+      let startDistance = 0
+
+      const touchStart = function (e) {
+        drag = true
+        oldX = e.changedTouches[0].pageX
+        oldY = e.changedTouches[0].pageY
+        e.preventDefault()
+        return false
+      }
+
+      const touchEnd = function () {
+        drag = false
+      }
+
+      const touchMove = function (e) {
+        if (!drag) return false
+        e.preventDefault()
+        if (event.changedTouches[1] == undefined) {// 单点触控
+          dX = (e.changedTouches[0].pageX - oldX) * 2 * Math.PI / gl.canvas.clientWidth
+          dY = (e.changedTouches[0].pageY - oldY) * 2 * Math.PI / gl.canvas.clientHeight
+          oldX = e.changedTouches[0].pageX
+          oldY = e.changedTouches[0].pageY
+          rotateCamera(dX, dY)
+          updateCamera()
+        } else {
+          currentDistance = Math.sqrt(Math.pow(e.changedTouches[1].pageX - e.changedTouches[0].pageX, 2) + Math.pow(e.changedTouches[1].pageY - e.changedTouches[0].pageY, 2))
+          zoomInCamera(startDistance - currentDistance)
+          startDistance = currentDistance
+        }
+      }
+
+      canvas.addEventListener('touchstart', touchStart, false)
+      canvas.addEventListener('touchend', touchEnd, false)
+      canvas.addEventListener('touchmove', touchMove, false)
+    }
+
+    supportedTouch ? bindTouchEvents() : bindMouseEvents()
   },
   beforeDestroy () {
     animationID && cancelAnimationFrame(animationID)
